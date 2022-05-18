e9d5620ca1de456ae7b98e71f19df001b22f0efd
omkarpatne6 <omkarpatne6@gmail.com>
2022-05-18 21:48:04 +0530

    make it better

--- conn.js
+++ conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://omkar:omkar123@test.ynj89.mongodb.net/exampledb', {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
+mongoose.connect('mongodb+srv://omkar:omkar123@test.ynj89.mongodb.net/exampledb', {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {
         console.log("connected to the db successfully")
     })
